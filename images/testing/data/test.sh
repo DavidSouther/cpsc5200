d4d17e59ce63cd10c03470545a9812eddc45326f
David Souther <davidsouther+github@gmail.com>
2021-02-19 11:05:57 -0800

    Simplify test script

--- images/testing/data/test.sh
+++ images/testing/data/test.sh
@@ -3,10 +3,9 @@
 DIRS=( boat carseat desk floor pillow pond )
 
 for D in ${DIRS[@]} ; do 
-    curl http://localhost:5000/photo -F 'file=@${D}/image.png' > ./id
-    ID=$(<./id)
+    ID=$(curl http://localhost:5000/photo -F 'file=@${D}/image.png')
     curl --header "Content-Type: application/json" \
         --request POST \
-        --data $(<${D}/convert.json) \
+        --data "@${D}/convert.json" \
         http://localhost:5000/${ID}:transform
 done
