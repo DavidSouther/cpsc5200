1b69082a47ebddce6ec3dfa98232a284a0fb109d
David Souther <davidsouther+github@gmail.com>
2021-02-19 12:12:03 -0800

    Run multiple tests in concurrent shells

--- images/testing/data/test.sh
+++ images/testing/data/test.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+ROOT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 DIRS=( boat carseat desk floor pillow pond )
 
-for D in ${DIRS[@]} ; do 
-    ID=$(curl http://localhost:5000/photo -F 'file=@${D}/image.png')
+echo "Reading test data from $ROOT"
+
+for D in ${DIRS[@]} ; do (
+    set -x
+    D="$ROOT/$D"
+    sleep $(python -c "import random; print(random.random())")
+
+    ID=$(curl -F "file=@${D}/image.png" http://localhost:5000/photo 2>/dev/null)
     curl --header "Content-Type: application/json" \
         --request POST \
         --data "@${D}/convert.json" \
-        http://localhost:5000/${ID}:transform
+        http://localhost:5000${ID}:transform
+) &
 done
+
+wait
